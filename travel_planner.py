f927e255ee78dea066c097b32aa0f47a49368cf5
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-04-07 11:46:07 -0400

    test

--- travel_planner.py
+++ travel_planner.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import json
 import logging
-from  sklearn.ensemble import GradientBoostingRegressor 
+from  sklearn.ensemble import GradientBoostingRegressor
 
 app = Flask(__name__)
 
@@ -40,31 +40,31 @@ def results():
             do_longitude = inputs['do_longitude'][0]
         except:
             do_longitude = -73.95
-	try:
-	    pu_hour = inputs['pu_hour'][0]
-	except:
-	    pu_hour = 12
-	try:
-	    pu_day = inputs['pu_day'][0]
-	except:
-	    pu_day = 5
-	try:
-            trip_distance = inputs['trip_distance'][0]
-	except:
-	    trip_distance = 10 
+    	try:
+    	    pu_hour = inputs['pu_hour'][0]
+    	except:
+    	    pu_hour = 12
+    	try:
+    	    pu_day = inputs['pu_day'][0]
+    	except:
+    	    pu_day = 5
+    	try:
+                trip_distance = inputs['trip_distance'][0]
+    	except:
+    	    trip_distance = 10
 
-        time_pred_array = np.array([is_yellow, pu_latitude,\
-            pu_longitude, do_latitude, do_longitude, pu_hour, pu_day])
+            time_pred_array = np.array([is_yellow, pu_latitude,\
+                pu_longitude, do_latitude, do_longitude, pu_hour, pu_day])
 
-        trip_time = time_model.predict(time_pred_array)[0]
+            trip_time = time_model.predict(time_pred_array)[0]
 
-        base_fare_array = np.array([trip_distance, trip_time])
+            base_fare_array = np.array([trip_distance, trip_time])
 
-        base_fare = base_fare_model.predict(base_fare_array)[0]
+            base_fare = base_fare_model.predict(base_fare_array)[0]
 
-        final_fare = base_fare
+            final_fare = base_fare
 
-        return jsonify(trip_time, final_fare)
+            return jsonify(trip_time, final_fare)
 
 
 if __name__ == '__main__':
